2123d55ea19dea142385ff1f5a760ecbf1e114f1
Pierre-Marie Primo <pierrem.primo@gmail.com>
2023-05-12 09:49:44 +0200

    switched email edition to RepeatedType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -9,16 +9,27 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email' , EmailType::class, [
-            "attr" => ["class" => "form-control"],
-            "label_attr" => ["class" => "editor__label"],
-            'label' => 'Nouvelle adresse mail'
+        ->add('email', RepeatedType::class, [
+            'type' => EmailType::class,
+            'invalid_message' => 'Les adresses mail doivent être identiques',
+            'required' => true,
+            'first_options' => [
+                'label' => 'Nouvelle adresse mail',
+                'label_attr' => ["class" => "editor__label"],
+                'attr' => ["class" => "form-control"]
+            ],
+            'second_options' => [
+                'label' => 'Confirmer l\'adresse mail',
+                'label_attr' => ["class" => "editor__label"],
+                'attr' => ["class" => "form-control"]
+            ],
         ])
         ->add('pseudo', TextType::class, [
             "attr" => ["class" => "form-control"],
